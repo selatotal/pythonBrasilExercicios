786609e76b43ad4b2e9b9f1272e879b876c76d5f
Tales Viegas <tales.viegas@corp.terra.com.br>
2014-07-02 00:03:40 -0300

    Exercícios de Funções

---
+++ 05_Funcoes/01_imprime.py
@@ -0,0 +1,8 @@
+def imprime ( n ):
+	for i in range(1, n + 1):
+		for j in range(1, i+1):
+			print i,
+		print
+
+numero = int(raw_input('Informe um numero: '))
+imprime (numero)
---
+++ 05_Funcoes/03_soma.py
@@ -0,0 +1,8 @@
+def soma ( valor1, valor2, valor3 ):
+	return valor1 + valor2 + valor3
+
+valor1 = int(raw_input('Informe o primeiro valor: '))
+valor2 = int(raw_input('Informe o segundo valor: '))
+valor3 = int(raw_input('Informe o terceiro valor: '))
+
+print 'A soma dos valores e %d' % soma(valor1, valor2, valor3)
