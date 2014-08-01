ca9d787c9dcf7d02cbab411b339560e263e6ad3a
Tales Viegas <tales.viegas@corp.terra.com.br>
2014-08-01 18:41:37 -0300

    Mais exercícios de String
    Exercícios 12 e 13 de String

---
+++ 06_Strings/12_valida_telefone.py
@@ -0,0 +1,9 @@
+print 'Valida e corrige numero de telefone'
+telefone = raw_input('Telefone: ')
+
+telefoneLimpo = telefone.replace('-', '')
+
+if (len(telefoneLimpo) == 7):
+	print 'Telefone possui 7 digitos. Vou acrescentar o digito tres na frente.'
+	print 'Telefone corrigido sem formatacao: 3%s' % telefoneLimpo
+	print 'Telefone corrigido com formatacao: 3%s' % telefone
---
+++ 06_Strings/13_forca_embaralhada.py
@@ -0,0 +1,26 @@
+import random
+
+# Abre o arquivo para leitura
+arquivoEntrada = open('11_palavras.txt', 'r')
+
+# Coloca todas as linhas em memoria
+palavras = arquivoEntrada.readlines()
+
+# Fecha o arquivo
+arquivoEntrada.close()
+
+palavraEscolhida = palavras[random.randint(0, len(palavras)-1)].upper().strip()
+tamanhoPalavra = len(palavraEscolhida)
+#palavraAdivinhada = [ '_' ] * tamanhoPalavra
+palavraAdivinhada = random.sample(palavraEscolhida, len(palavraEscolhida))
+for i in palavraAdivinhada:
+	print '%s' % i,
+print
+
+palavra = raw_input('Digite qual eh a palavra: ').upper()
+
+if (palavra == palavraEscolhida):
+	print 'Voce acertou!'
+else:
+	print 'Voce perdeu!'
+	print 'A palavra era %s' % palavraEscolhida
