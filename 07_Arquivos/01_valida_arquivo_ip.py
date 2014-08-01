f978fde2324f7fbd27cb3717ad5d476747117172
Tales Viegas <tales.viegas@corp.terra.com.br>
2014-08-01 20:01:50 -0300

    Strings e Arquivos
    Finalização dos exercícios de Strings e arquivos

---
+++ 07_Arquivos/01_valida_arquivo_ip.py
@@ -0,0 +1,35 @@
+def validaIP(ipAddress):
+	blocos = ipAddress.split('.')
+	if (len(blocos) != 4):
+		return False
+	for i in blocos:
+		i_i = int(i)
+		if (i_i < 0) or (i_i > 255):
+			return False
+	return True
+
+# FLUXO PRINCIPAL
+arquivoEntrada = open('01_entrada.txt', 'r')
+linhas = arquivoEntrada.readlines()
+arquivoEntrada.close()
+
+ipsValidos = []
+ipsInvalidos = []
+
+for ip in linhas:
+	if (validaIP(ip.strip())):
+		ipsValidos.append(ip.strip())
+	else:
+		ipsInvalidos.append(ip.strip())
+
+arquivoSaida = open('01_saida.txt', 'w')
+
+arquivoSaida.write('[IPs Validos]\n')
+for ip in ipsValidos:
+	arquivoSaida.write('%s\n' % ip)
+
+arquivoSaida.write('\n[IPs Invalidos]\n')
+for ip in ipsInvalidos:
+	arquivoSaida.write('%s\n' % ip)
+
+arquivoSaida.close()
---
+++ 07_Arquivos/02_espaco_disco.py
@@ -0,0 +1,37 @@
+# Abre o arquivo para leitura
+arquivoEntrada = open('02_entrada.txt', 'r')
+
+# Coloca todas as linhas em memoria
+linhas = arquivoEntrada.readlines()
+
+# Fecha o arquivo
+arquivoEntrada.close()
+
+usuarios = []
+espacosUtilizados = []
+espacoTotal = 0.0
+for linha in linhas:
+	campos = linha.split()
+	usuario = campos[0]
+	espacoUtilizado = int(campos[1])
+	usuarios.append(usuario)
+	espacosUtilizados.append(espacoUtilizado)
+	espacoTotal += espacoUtilizado
+
+# Abre o arquivo para escrita
+arquivoSaida = open('02_saida.txt', 'w')
+
+arquivoSaida.write('ACME Inc.               Uso do espaco em disco pelos usuarios')
+arquivoSaida.write('\n------------------------------------------------------------------------')
+arquivoSaida.write('\nNr.  Usuario        Espaco utilizado     %% do uso')
+
+for i in range(0, len(usuarios)):
+	espacoMB = espacosUtilizados[i] / (1024.0*1024.0)
+	percentualUso = espacosUtilizados[i] / espacoTotal
+	arquivoSaida.write('\n%d - %s - %.2f MB - %.2f%%' % (i+1, usuarios[i], espacoMB, percentualUso*100.0))
+
+arquivoSaida.write('\nEspaco total ocupado: %.2f MB' % (espacoTotal/(1024.0*1024.0)))
+arquivoSaida.write('\nEspaco medio ocupado: %.2f MB' % (espacoTotal / len(usuarios) / (1024.0*1024.0)))
+
+# Fecha o arquivo
+arquivoSaida.close()
