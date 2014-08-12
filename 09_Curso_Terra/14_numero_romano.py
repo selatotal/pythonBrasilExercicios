e426a981dd5ea4b49ab29962d2f517cc84ec9c1d
Tales Viegas <tales.viegas@corp.terra.com.br>
2014-08-12 11:01:16 -0300

    Correções PEP8 no exercício 14

--- 09_Curso_Terra/14_numero_romano.py
+++ 09_Curso_Terra/14_numero_romano.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 class NumeroRomano():
 
     def __init__(self, numero):
-        self.mapa = { 'V': 5, 'X': 10 }
+        self.mapa = {'V': 5, 'X': 10}
         self.numero = self.mapa[numero]
         self.romano = numero
 
@@ -17,8 +18,8 @@ class NumeroRomano():
     def __str__(self):
         return self.romano
 
-num1 = NumeroRomano('X') # 10
-num2 = NumeroRomano('V') # 5
+num1 = NumeroRomano('X')  # 10
+num2 = NumeroRomano('V')  # 5
 
 print int(num1)
 print int(num2)
