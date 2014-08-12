2416c4190cdbfe467e7c45f9379a6fdd6789f692
Tales Viegas <selatotal@gmail.com>
2014-08-12 13:07:37 -0300

    Exercicio 14

---
+++ 09_Curso_Terra/16_flask/facter_app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request
+from facter_utils import get_argument
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('facter.html', fields=None )
+
+@app.route('/', methods=['POST'])
+def index_post():
+    name = request.form.get('name')
+    value = get_argument(name)
+    fields = { 'name' : name, 'value' : value }
+    return render_template('facter.html', fields=fields)
+    
+app.run(debug=True)
